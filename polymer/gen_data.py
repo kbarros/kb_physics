4d81c6b747b5c7e0a9982241074a56276444288d
Kipton Barros <kbarros@achilles.ms.northwestern.edu>
2009-08-20 20:51:11 -0500

    now generate entire direcotry

--- polymer/gen_data.py
+++ polymer/gen_data.py
@@ -1,18 +1,105 @@
 #!/usr/bin/python
 
-N = 16
-rho = 0.008
-L = (N / rho)**(1./3.) # approx 12.6
+import os
 
 
-header = ("""LAMMPS FENE chain data file
- 
+N = 16 # chain length
+Ns = 1 # salt molecules
+Z = 1 # salt valency
+rho = 0.008 # monomer density
+L = (N / rho)**(1./3.) # linear system size
+
+simsteps = 50000
+dumpevery = 100
+num_dumps = simsteps / dumpevery
+
+dirname = "N%d.Ns%d.Z%d.rho%s##" % (N, N, Z, str(rho))
+confname = "conf.dat"
+dataname = "data.dat"
+chaincfgname = "chaincfg.dat"
+cmdsname = "cmds.dat"
+dumpname = "dump.dat"
+analysisname = "analysis.dat"
+
+
+# -----------------------------------------------------------------
+# Create a new directory, with a possible suffix for uniqueness.
+# Return the name of the newly created directory.
+#
+
+def mknewdir(newdir):
+    if os.path.exists(newdir):
+        suffix = 1
+        while os.path.exists(newdir+"-"+str(suffix)):
+            suffix += 1
+        newdir += "-"+str(suffix)
+    os.mkdir(newdir)
+    return newdir
+
+
+# -----------------------------------------------------------------
+# Generate LAMMPS config file
+#
+
+def generate_conf_file():
+    return (
+"""# automatically generated polymer configuration file
+
+units		lj
+boundary	p p p
+dimension	3
+
+atom_style	full
+neighbor	0.3 bin
+neigh_modify	delay 5
+
+read_data	%(dataname)s		# load volume dimensions, masses, atoms, and bonds
+
+pair_style	lj/cut/coul/long 1.12246204830937 12 # LJ_cutoff=2^{1/6}  [ coulomb_cutoff ]
+pair_coeff	* * 1.0 1.0		# < atom_type1 atom_type2 epsilon sigma [ LJ_cutoff coulomb_cutoff ] >
+pair_modify	shift yes		# LJ interactions shifted to zero
+kspace_style	ewald 1.0e-4		# desired accuracy
+# LAMMPS coulombic energy is (q1 q2) / (\eps r)
+# bjerrum length is (\lambda_B = 3)
+# temperature is (k T = 1.2)
+# so dielectric is (1 / (1.2 * 3))
+dielectric	0.2777777777777
+
+bond_style	fene
+bond_coeff	* 7 2 0 0		# < bond_type K R0 fene_epsilon fene_sigma >
+special_bonds	lj/coul 1.0 1.0 1.0  	# turn on regular interactions between all monomers (even bonded ones)
+
+# apply newtons equations
+fix		1 all nve
+fix		2 all langevin 1.2 1.2 10.0 699483 # < ID group-ID langevin Tstart Tstop damp seed [keyword values ... ] >
+#fix		3 all setforce NULL NULL 0.0	   # enforce 2d
+
+# run
+timestep	0.003
+thermo		%(dumpevery)s				# output thermodynamic quantities every 1000 steps
+dump		1 all atom %(dumpevery)s %(dumpname)s	# < ID group-ID style every_N_timesteps file args >
+#dump_modify	1 image yes
+dump_modify	1 image yes scale no
+
+run		%(simsteps)s
+""" % {'dataname':dataname, 'dumpname':dumpname, 'simsteps':simsteps, 'dumpevery':dumpevery})
+
+
+
+# -----------------------------------------------------------------
+# Generate input data file
+#
+
+def generate_data_file():
+    header = (
+"""LAMMPS FENE chain data file
+
           %(natoms)d  atoms
           %(nbonds)d  bonds
            0  angles
            0  dihedrals
            0  impropers
- 
+
            2  atom types
            1  bond types
            0  angle types
@@ -24,61 +111,108 @@ header = ("""LAMMPS FENE chain data file
  %(lmin)f  %(lmax)f zlo zhi
 
 """ % {'natoms':2*N, 'nbonds':(N-1), 'lmin':0, 'lmax':L})
-
-
-masses = (
+    
+    masses = (
 """Masses
   #
   1  1.0
   2  1.0
 
 """)
+    
+    def wrap_coordinate(x):
+        return (x % L, x / L) # todo: handle negative winding numbers
+    
+    def atom_position(i):
+        mx = 1
+        my = (1 / L)
+        mz = (1 / L**2)
+        x = L/2. + i*mx
+        y = L/2. + i*my
+        z = L/2. + i*mz
+        return (x,y,z)
+    
+    def atom_str(atom_id, molecule_id, atom_type, charge):
+        (x,ix), (y,iy), (z,iz) = map(wrap_coordinate, atom_position(atom_id))
+        return "  %d  %d  %d  %f  %f  %f  %f  %d  %d  %d\n" % (atom_id, molecule_id, atom_type, charge, x, y, z, ix, iy, iz)
+    
+    atoms = "Atoms\n  # atom_id, molecule_id, atom_type, charge, x, y, z, wind_x, wind_y, wind_z \n"
+    # create polymer chain
+    for i in range(N):
+        atom_id = i+1
+        molecule_id = 1
+        atom_type = 1
+        charge = -1
+        atoms += atom_str(atom_id, molecule_id, atom_type, charge)
+    # create ions
+    for i in range(N):
+        atom_id = N+(i+1)
+        molecule_id = 2
+        atom_type = 2
+        charge = 1
+        atoms += atom_str(atom_id, molecule_id, atom_type, charge)
+    atoms += "\n"
+    
+    bonds = "Bonds\n  # bond_id, molecule_id, atom1_id, atom2_id\n"
+    for i in range(N-1):
+        bond_id = i+1
+        molecule_id = 1
+        bonds += "  %d  %d  %d  %d\n" % (bond_id, molecule_id, bond_id, bond_id+1)
+    bonds += "\n"
+    
+    return header + masses + atoms + bonds
+
+
+# -----------------------------------------------------------------
+# Generate configuration file for chain analysis
+#
+
+def generate_chain_config_file():
+    return (
+"""dumpname       = %s
+timesteps      = %d
+maxdeltat      = 1
+chainlength    = %d
+no_chains      = 1
+ad_condition_z = 1 
+compute_msd    = 0 
+outputfile     = %s
+adsorb         = adsorb.dat 
+msdout         = msd.dat
+""" % (dumpname, num_dumps, N, analysisname))
+
+
+# -----------------------------------------------------------------
+# Generate commands file
+#
+
+def generate_cmds_file():
+    return (
+"""
+lamps < %s
+chain_analysis chaincfg.dat
+generic_analyzer analysis.dat 8
+""" % confname)
 
 
 
-def wrap_coordinate(x):
-    return (x % L, x / L) # todo: handle negative winding numbers
+# -----------------------------------------------------------------
+# Build a new simulation directory
+#
 
-def atom_position(i):
-    mx = 1
-    my = (1 / L)
-    mz = (1 / L**2)
-    x = L/2. + i*mx
-    y = L/2. + i*my
-    z = L/2. + i*mz
-    return (x,y,z)
+def build_sim_dir():
+    root = mknewdir(dirname) + "/"
+    
+    with open(root + confname, 'w') as f:
+        f.write(generate_conf_file())
+    
+    with open(root + dataname, 'w') as f:
+        f.write(generate_data_file())
+    
+    with open(root + chaincfgname, 'w') as f:
+        f.write(generate_chain_config_file())
 
+    with open(root + cmdsname, 'w') as f:
+        f.write(generate_cmds_file())
 
-def atom_str(atom_id, molecule_id, atom_type, charge):
-    (x,ix), (y,iy), (z,iz) = map(wrap_coordinate, atom_position(atom_id))
-    return "  %d  %d  %d  %f  %f  %f  %f  %d  %d  %d\n" % (atom_id, molecule_id, atom_type, charge, x, y, z, ix, iy, iz)
-
-
-atoms = "Atoms\n  # atom_id, molecule_id, atom_type, charge, x, y, z, wind_x, wind_y, wind_z \n"
-# create polymer chain
-for i in range(N):
-    atom_id = i+1
-    molecule_id = 1
-    atom_type = 1
-    charge = -1
-    atoms += atom_str(atom_id, molecule_id, atom_type, charge)
-# create ions
-for i in range(N):
-    atom_id = N+(i+1)
-    molecule_id = 2
-    atom_type = 2
-    charge = 1
-    atoms += atom_str(atom_id, molecule_id, atom_type, charge)
-atoms += "\n"
-
-
-bonds = "Bonds\n  # bond_id, molecule_id, atom1_id, atom2_id\n"
-for i in range(N-1):
-    bond_id = i+1
-    molecule_id = 1
-    bonds += "  %d  %d  %d  %d\n" % (bond_id, molecule_id, bond_id, bond_id+1)
-bonds += "\n"
-
-
-print (header + masses + atoms + bonds)
-
+build_sim_dir()
