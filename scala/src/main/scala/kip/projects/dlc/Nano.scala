7d13e39d9169794c2e8ae3f44041f70e0f04738d
Kipton Barros <kbarros@bu.edu>
2010-05-11 11:57:44 -0500

    Reorg block analysis, compute error in pair correlation

--- scala/src/main/scala/kip/projects/dlc/Nano.scala
+++ scala/src/main/scala/kip/projects/dlc/Nano.scala
@@ -19,11 +19,16 @@ object Nano {
   def averageTemperature(snaps: Seq[Snapshot]) = {
     snaps.map(_.thermo.temperature).sum / snaps.size
   }
+  
+  def pairCorrelationBins(dr: Double, rmax: Double) = {
+    val nbins = (rmax/dr).toInt
+    Array.range(0, nbins).map(bin => (bin + 0.5) * dr)
+  }
 
   def pairCorrelation(snaps: Seq[Snapshot], dr: Double, rmax: Double, ids1: Seq[Int], ids2: Seq[Int]) = {
     val volume = snaps(0).volume
-    val nbins = (rmax/dr).toInt
-    val g = new Array[Double](nbins)
+    val r = pairCorrelationBins(dr, rmax)
+    val g = new Array[Double](r.size)
     
     // sum over all snapshots, and all pairs of particles. distances are binned into g
     for ((s,iter) <- snaps.zipWithIndex) {
@@ -32,23 +37,33 @@ object Nano {
       for (i1 <- ids1; i2 <- ids2; if i1 != i2) {
         val dist = math.sqrt(s.distance2(i1, i2))
         val bin = (dist/dr).toInt
-        if (bin < nbins)
+        if (bin < r.size)
           g(bin) += 1
       }
     }
 
-    // bin radii (average)
-    val r = Array.range(0, nbins).map(bin => (bin + 0.5) * dr)
-    
     // normalize pair-correlation so that it becomes unity at homogeneous density
     val uniquePairs = (ids1.size * ids2.size) - (ids1.toSet & ids2.toSet).size
-    for (bin <- 0 until nbins) {
+    for (bin <- 0 until r.size) {
       val volume_fraction = 4*math.Pi*r(bin)*r(bin)*dr / volume
       g(bin) /= volume_fraction * snaps.size * uniquePairs
     }
     
-    (r, g)
+    g
   }
+  
+  def pairCorrelationWithError(snaps: Seq[Snapshot], dr: Double, rmax: Double, ids1: Seq[Int], ids2: Seq[Int]) = {
+    // partition snapshots into 199 equal sized groups
+    val snapsGrouped = snaps.grouped(snaps.size / 200).toArray.dropRight(1)
+    
+    // for each distance 'r', construct array of approximations to g(r)
+    val gs = snapsGrouped.map(pairCorrelation(_, dr, rmax, ids1, ids2)).transpose
+    
+    // estimate pair correlation as g_mean +- g_err
+    val analysis = gs.map (new kip.util.BlockAnalysis(_))
+    (analysis.map(_.mean), analysis.map(_.error))
+  }
+
   
   def go(fname: String, tbegin: Long, dr: Double, rmax: Double) {
     val snaps = LammpsParser.readLammpsDump(fname) filter {_.time > tbegin}
@@ -58,25 +73,25 @@ object Nano {
     
     val s = snaps(0)
     val types = snaps(0).typ
-    def isPositive(q: Array[Double], i: Int) = if (q == null) true else q(i) > 0
-    def isNegative(q: Array[Double], i: Int) = if (q == null) true else q(i) < 0
     def filterIds(f: Int => Boolean) = (0 until s.natoms) filter f
     
-    val idsCorePos = filterIds (i => s.typ(i) == typCore && isPositive(s.q, i))
-    val idsCoreNeg = filterIds (i => s.typ(i) == typCore && isNegative(s.q, i))
     val idsCore    = filterIds (i => s.typ(i) == typCore)
-    val idsCation  = filterIds (i => s.typ(i) == typCation)
+    val idsCation  = filterIds (i => s.typ(i) == typCation || s.typ(i) == typSphereCation)
     val idsAnion   = filterIds (i => s.typ(i) == typAnion)
     
-    val (r, g1) = pairCorrelation(snaps, dr, rmax, idsCorePos, idsCoreNeg)
-    val (_, g2) = pairCorrelation(snaps, dr, rmax, idsCore, idsCation)
-    val (_, g3) = pairCorrelation(snaps, dr, rmax, idsCation, idsCation)
+    val r  = pairCorrelationBins(dr, rmax)
+    val (g1,e1) = pairCorrelationWithError(snaps, dr, rmax, idsCore, idsCore)
+    val (g2,e2) = pairCorrelationWithError(snaps, dr, rmax, idsCore, idsCation)
+    val (g3,e3) = pairCorrelationWithError(snaps, dr, rmax, idsCation, idsCation)
     
     val formatted = Util.formatDataInColumns(
       ("radii", r),
       ("g(core-core)", g1),
+      ("err", e1),
       ("g(core-cation)", g2),
-      ("g(cation-cation)", g3)
+      ("err", e2),
+      ("g(cation-cation)", g3),
+      ("err", e3)
     )
     Util.writeStringToFile(formatted, "results.dat")
     // print(formatted)
---
+++ scala/src/main/scala/kip/util/BlockAnalysis.scala
@@ -0,0 +1,69 @@
+package kip.util
+
+
+class BlockAnalysis(data: Array[Double]) {
+  val n = data.size
+  val mean = data.sum / n
+  
+  val (error, error_error) = {
+    blocks.find(_.corr < 0.1) match {
+      case Some(iter) => (iter.sigma, iter.sigma_err)
+      case None       => (blocks.last.sigma, Double.PositiveInfinity)
+    }
+  }
+  
+  case class Block(sigma: Double, sigma_err: Double, corr: Double, n: Int)
+  
+  val blocks = {
+    var a = data
+    var n = a.size
+    val mean = a.sum / n
+    var ret: List[Block] = Nil
+    
+    while (n > 8) {
+      val c0 = c_t(a, 0)
+      val c1 = c_t(a, 1)
+
+      // see Flyvbjerb & Petersen, 1989
+      val sigma2 = c0 / (n - 1.0)
+      val sigma2_err = sigma2 * math.sqrt(2.0 / (n - 1.0))
+      
+      // see Propagation of Uncertainty, Wikipedia,
+      // (x = a +- e) and (y = x^c) => (y = a^c +- c e (a^c / a))
+      val sigma = math.sqrt(sigma2)
+      val sigma_err = 0.5 * sigma2_err * sigma / sigma2
+
+      val corr = c1 / c0
+      ret = Block(sigma, sigma_err, corr, n) +: ret
+
+      a = decimate(a, 2)
+      n = a.size
+    }
+    
+    // finest blocking is first in return list
+    ret.reverse
+  }
+
+  private def decimate(a: Array[Double], factor: Int): Array[Double] = {
+    val n = a.size
+    val ret = new Array[Double](n / factor)
+    for (i <- ret.indices) {
+      ret(i) = 0
+      for (j <- 0 until factor) {
+        ret(i) += a(factor*i+j)/factor
+      }
+    }
+    ret
+  }
+  
+  private def c_t(a: Array[Double], t: Int) = {
+    val n = a.size
+    var ret = 0.0
+    for (i <- 0 until (n-t)) {
+      val del1 = a(i+0) - mean
+      val del2 = a(i+t) - mean
+      ret += del1*del2
+    }
+    ret / (n-t)
+  }
+}
