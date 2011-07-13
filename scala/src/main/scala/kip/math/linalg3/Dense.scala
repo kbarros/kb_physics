522375681103837da379197c1032adebc5bcb701
Kipton Barros <kbarros@gmail.com>
2011-07-13 11:46:32 -0600

    Start adding generic multiplication

--- scala/src/main/scala/kip/math/linalg3/Dense.scala
+++ scala/src/main/scala/kip/math/linalg3/Dense.scala
@@ -3,6 +3,11 @@ package kip.math.linalg3
 import kip.math.Complex
 import kip.math.linalg2.{ScalarOps, ScalarData}
 
+
+// TODO: put implicits in object Dense 
+
+
+
 // Matrix types
 
 trait Dense[A, Raw] extends Matrix[A, Raw, Dense] {
@@ -30,47 +35,95 @@ trait DenseCol[A, Raw] extends Matrix[A, Raw, DenseCol] with Dense[A, Raw]
 
 // Adders
 
-trait DenseAdders1 {
+trait DenseAdders {
+  private def genericAddInPlace[A, Raw](sub: Boolean, m1: Dense[A, Raw], m2: Dense[A, Raw], ret: Dense[A, Raw]) {
+    require(ret.numRows == m1.numRows && ret.numRows == m2.numRows, "Mismatched rows: %d, %d, %d".format(m1.numRows, m2.numRows, ret.numRows))
+    require(ret.numCols == m1.numCols && ret.numCols == m2.numCols, "Mismatched cols: %d, %d, %d".format(m1.numCols, m2.numCols, ret.numCols))
+    for ((i, j) <- ret.indices) ret(i, j) =
+      if (sub) ret.scalar.sub(m1(i, j), m2(i, j)) else ret.scalar.add(m1(i, j), m2(i, j))
+  }
+  
   trait DDAdder[A, Raw] extends MatrixAdder[A, Raw, Dense, Dense, Dense] {
-    def addInPlace(neg1: Boolean, m1: Dense[A, Raw], neg2: Boolean, m2: Dense[A, Raw], ret: Dense[A, Raw]) {
-      println("dense dense adder")
-    }
+    def addInPlace(sub: Boolean, m1: Dense[A, Raw], m2: Dense[A, Raw], ret: Dense[A, Raw]) =
+      genericAddInPlace(sub, m1, m2, ret)
   }
   implicit def dda[A, Raw] = new DDAdder[A, Raw] {}
-}
 
-trait DenseAdders0 extends DenseAdders1 {
   trait DRAdder[A, Raw] extends MatrixAdder[A, Raw, Dense, DenseRow, DenseRow] {
-    def addInPlace(neg1: Boolean, m1: Dense[A, Raw], neg2: Boolean, m2: DenseRow[A, Raw], ret: DenseRow[A, Raw]) {
-    }
+    def addInPlace(sub: Boolean, m1: Dense[A, Raw], m2: DenseRow[A, Raw], ret: DenseRow[A, Raw]) =
+      genericAddInPlace(sub, m1, m2, ret)
   }
   implicit def dra[A, Raw] = new DRAdder[A, Raw] {}
 
   trait RDAdder[A, Raw] extends MatrixAdder[A, Raw, DenseRow, Dense, DenseRow] {
-    def addInPlace(neg1: Boolean, m1: DenseRow[A, Raw], neg2: Boolean, m2: Dense[A, Raw], ret: DenseRow[A, Raw]) {
-    }
+    def addInPlace(sub: Boolean, m1: DenseRow[A, Raw], m2: Dense[A, Raw], ret: DenseRow[A, Raw]) =
+      genericAddInPlace(sub, m1, m2, ret)
   }
   implicit def rda[A, Raw] = new RDAdder[A, Raw] {}
-}
 
-trait DenseAdders extends DenseAdders0{
   trait RRAdder[A, Raw] extends MatrixAdder[A, Raw, DenseRow, DenseRow, DenseRow] {
-    override def addInPlace(neg1: Boolean, m1: DenseRow[A, Raw], neg2: Boolean, m2: DenseRow[A, Raw], ret: DenseRow[A, Raw]) {
-      println("r r adder")
-    }
+    def addInPlace(sub: Boolean, m1: DenseRow[A, Raw], m2: DenseRow[A, Raw], ret: DenseRow[A, Raw]) =
+      genericAddInPlace(sub, m1, m2, ret)
   }
   implicit def rra[A, Raw] = new RRAdder[A, Raw] {}
 }
 
 
+// Multipliers
+
+trait DenseMultipliers {
+  private def genericGemm[A, Raw](alpha: A, beta: A, m1: Dense[A, Raw], m2: Dense[A, Raw], ret: Dense[A, Raw]) {
+    require(ret.numRows == m1.numRows &&
+            m1.numCols == m2.numRows &&
+            m2.numCols == ret.numCols, "Cannot multiply matrices: (%d, %d) * (%d, %d) -> (%d, %d)".format(
+              m1.numRows, m1.numCols, m2.numRows, m2.numCols, ret.numRows, ret.numCols))
+
+//    if (Netlib.cblas == null) {
+      for (i <- 0 until ret.numRows;
+           k <- 0 until m1.numCols;
+           j <- 0 until ret.numCols) {
+        ret.data.madd(ret.index(i, j), m1.data, m1.index(i, k), m2.data, m2.index(k, j))
+      }
+//    }
+/*
+    else {
+      netlib.gemm(netlib.CblasColMajor, netlib.CblasNoTrans, netlib.CblasNoTrans,
+                  ret.numRows, ret.numCols, // dimension of return matrix
+                  numCols, // dimension of summation index
+                  scalar.one, // alpha
+                  data, numRows, // A matrix
+                  that.data, that.numRows, // B matrix
+                  scalar.zero, // beta
+                  ret.data, ret.numRows // C matrix
+                )
+     }
+*/
+  }
+  
+  trait DDMultiplier[A, Raw] extends MatrixMultiplier[A, Raw, Dense, Dense, Dense] {
+    def gemm(alpha: A, beta: A, m1: Dense[A, Raw], m2: Dense[A, Raw], ret: Dense[A, Raw]) =
+      genericGemm(alpha, beta, m1, m2, ret)
+  }
+  implicit def ddm[A, Raw] = new DDMultiplier[A, Raw] {}
+
+  trait DRMultiplier[A, Raw] extends MatrixMultiplier[A, Raw, Dense, DenseRow, DenseRow] {
+    def gemm(alpha: A, beta: A, m1: Dense[A, Raw], m2: DenseRow[A, Raw], ret: DenseRow[A, Raw]) =
+      genericGemm(alpha, beta, m1, m2, ret)
+  }
+  implicit def drm[A, Raw] = new DDMultiplier[A, Raw] {}
+}
+
+
 // Builders
 trait DenseBuilders {
+
   implicit def dense[A, Raw](implicit sb: ScalarData.Builder[A, Raw]) = new MatrixBuilder[A, Raw, Dense] {
     def zeros(numRows: Int, numCols: Int) = {
+      require(numRows > 0 && numCols > 0, "Can't build matrix with non-positive dimensions (%d, %d)".format(numRows, numCols))
       val nr = numRows
       val nc = numCols
       new Dense[A, Raw] {
-        val data: ScalarData[A, Raw] = sb.build(numRows*numCols)
+        val data: ScalarData[A, Raw] = sb.build(nr*nc)
         val scalar: ScalarOps[A] = data.scalar
         val numRows = nr
         val numCols = nc
@@ -80,13 +133,28 @@ trait DenseBuilders {
   
   implicit def denseRow[A, Raw](implicit sb: ScalarData.Builder[A, Raw]) = new MatrixBuilder[A, Raw, DenseRow] {
     def zeros(numRows: Int, numCols: Int) = {
-      val nr = numRows
+      require(numRows > 0 && numCols > 0, "Can't build matrix with non-positive dimensions (%d, %d)".format(numRows, numCols))
+      require(numRows == 1, "Can't build row matrix with %d rows".format(numRows))
       val nc = numCols
       new DenseRow[A, Raw] {
-        val data: ScalarData[A, Raw] = sb.build(numRows*numCols)
+        val data: ScalarData[A, Raw] = sb.build(1*nc)
+        val scalar: ScalarOps[A] = data.scalar
+        val numRows = 1
+        val numCols = nc
+      }
+    }
+  }
+  
+  implicit def denseCol[A, Raw](implicit sb: ScalarData.Builder[A, Raw]) = new MatrixBuilder[A, Raw, DenseCol] {
+    def zeros(numRows: Int, numCols: Int) = {
+      require(numRows > 0 && numCols > 0, "Can't build matrix with non-positive dimensions (%d, %d)".format(numRows, numCols))
+      require(numCols == 1, "Can't build column matrix with %d rows".format(numCols))
+      val nr = numRows
+      new DenseCol[A, Raw] {
+        val data: ScalarData[A, Raw] = sb.build(nr*1)
         val scalar: ScalarOps[A] = data.scalar
         val numRows = nr
-        val numCols = nc
+        val numCols = 1
       }
     }
   }
--- scala/src/main/scala/kip/math/linalg3/Matrix.scala
+++ scala/src/main/scala/kip/math/linalg3/Matrix.scala
@@ -12,44 +12,152 @@ trait Matrix[A, Raw, +Repr[C, D] <: Matrix[C, D, Repr]] { self: Repr[A, Raw] =>
   def update(i: Int, j: Int, x: A)
   def indices: Iterator[(Int, Int)]
 
-  def clone[That[C,D] >: Repr[C,D]](implicit mb: MatrixBuilder[A, Raw, That]): That[A, Raw] = {
+
+  def mapTo[A2, Raw2, That[C,D] >: Repr[C,D] <: Matrix[C, D, That]](f: A => A2, that: That[A2, Raw2]): Unit =
+    indices.foreach { case(i, j) => that(i, j) = f(this(i, j)) }
+  def copyTo[That[C,D] >: Repr[C,D] <: Matrix[C, D, That]](that: That[A, Raw]): Unit =
+    mapTo(identity, that)
+  def mulTo[That[C,D] >: Repr[C,D] <: Matrix[C, D, That]](x: A, that: That[A, Raw]): Unit =
+    mapTo(scalar.mul(_, x), that)
+  def divTo[That[C,D] >: Repr[C,D] <: Matrix[C, D, That]](x: A, that: That[A, Raw]): Unit =
+    mapTo(scalar.div(_, x), that)
+  def negTo[That[C,D] >: Repr[C,D] <: Matrix[C, D, That]](that: That[A, Raw]): Unit =
+    mapTo(scalar.neg(_), that)
+  def conjTo[That[C,D] >: Repr[C,D] <: Matrix[C, D, That]](that: That[A, Raw]): Unit =
+    mapTo(scalar.conj(_), that)
+
+
+  def map[A2, Raw2, That[C,D] >: Repr[C,D] <: Matrix[C, D, That]]
+      (f: A => A2)(implicit mb: MatrixBuilder[A2, Raw2, That]): That[A2, Raw2] = {
     val ret = mb.zeros(numRows, numCols)
-//    copyTo(ret)
+    mapTo(f, ret)
+    ret
+  }
+
+  def duplicate[That[C,D] >: Repr[C,D] <: Matrix[C, D, That]](implicit mb: MatrixBuilder[A, Raw, That]): That[A, Raw] = {
+    val ret = mb.zeros(numRows, numCols)
+    copyTo(ret)
     ret
   }
   
-  def +[Repr2[C, D] <: Matrix[C, D, Repr2], Repr3[C, D] <: Matrix[C, D, Repr3]]
-        (that: Repr2[A, Raw])
-        (implicit ma: MatrixAdder[A, Raw, Repr, Repr2, Repr3],
-                  mb: MatrixBuilder[A, Raw, Repr3]): Repr3[A, Raw] = {
-    require(numCols == that.numRows)
-    val ret = mb.zeros(numRows, numCols)
-    ma.addInPlace(false, this, false, that, ret)
+  def *[That[C, D] >: Repr[C, D] <: Matrix[C, D, That]](x: A)(implicit mb: MatrixBuilder[A, Raw, That]): That[A, Raw] = {
+    val ret = duplicate(mb)
+    ret.mulTo(x, ret)
     ret
   }
 
-  def get: Repr[A, Raw] = this
+  def /[That[C, D] >: Repr[C, D] <: Matrix[C, D, That]](x: A)(implicit mb: MatrixBuilder[A, Raw, That]): That[A, Raw] = {
+    val ret = duplicate(mb)
+    ret.divTo(x, ret)
+    ret
+  }
 
+  def unary_-[That[C, D] >: Repr[C, D] <: Matrix[C, D, That]](implicit mb: MatrixBuilder[A, Raw, That]): That[A, Raw] = {
+    val ret = duplicate(mb)
+    ret.negTo(ret)
+    ret
+  }
+
+  def conj[That[C, D] >: Repr[C, D] <: Matrix[C, D, That]](implicit mb: MatrixBuilder[A, Raw, That]): That[A, Raw] = {
+    val ret = duplicate(mb)
+    ret.conjTo(ret)
+    ret
+  }
+
+/*
+  def tran(implicit mb: MatrixBuilder[A, Raw, Repr]) = {
+    val ret = clone
+    ret.transposeInPlace()
+    ret
+  }
+  
+  def dag(implicit mb: MatrixBuilder[A, Raw, Repr]) = {
+    val ret = tran
+    ret.conjugateInPlace()
+    ret
+  }
+*/
+
+  def *[Repr1[C, D] >: Repr[C, D], Repr2[C, D] <: Matrix[C, D, Repr2], Repr3[C, D] <: Matrix[C, D, Repr3]]
+        (that: Repr2[A, Raw])
+        (implicit ma: MatrixMultiplier[A, Raw, Repr1, Repr2, Repr3],
+                  mb: MatrixBuilder[A, Raw, Repr3]): Repr3[A, Raw] = {
+    require(numCols == that.numRows,
+            "Can't multiply matrices of dimensions (%d, %d) and (%d, %d)".format(numRows, numCols, that.numRows, that.numCols))
+    val ret = mb.zeros(numRows, numCols)
+    ma.gemm(scalar.one, scalar.zero, this, that, ret)
+    ret
+  }
+
+  def +[Repr1[C, D] >: Repr[C, D], Repr2[C, D] <: Matrix[C, D, Repr2], Repr3[C, D] <: Matrix[C, D, Repr3]]
+        (that: Repr2[A, Raw])
+        (implicit ma: MatrixAdder[A, Raw, Repr1, Repr2, Repr3],
+                  mb: MatrixBuilder[A, Raw, Repr3]): Repr3[A, Raw] = {
+    require(numRows == that.numRows && numCols == that.numCols,
+            "Can't add matrices of dimensions (%d, %d) and (%d, %d)".format(numRows, numCols, that.numRows, that.numCols))
+    val ret = mb.zeros(numRows, numCols)
+    ma.addInPlace(false, this, that, ret)
+    ret
+  }
+
+  def -[Repr1[C, D] >: Repr[C, D], Repr2[C, D] <: Matrix[C, D, Repr2], Repr3[C, D] <: Matrix[C, D, Repr3]]
+        (that: Repr2[A, Raw])
+        (implicit ma: MatrixAdder[A, Raw, Repr1, Repr2, Repr3],
+                  mb: MatrixBuilder[A, Raw, Repr3]): Repr3[A, Raw] = {
+    require(numRows == that.numRows && numCols == that.numCols,
+            "Can't subtract matrices of dimensions (%d, %d) and (%d, %d)".format(numRows, numCols, that.numRows, that.numCols))
+    val ret = mb.zeros(numRows, numCols)
+    ma.addInPlace(true, this, that, ret)
+    ret
+  }
+  
+  override def toString = {
+    val sb = new StringBuilder()
+    val elemWidth = 8
+    def writeStr(str: String) {
+      val spaces = Seq.fill(math.max(2, elemWidth-str.size))(' ').mkString
+      sb.append(spaces)
+      sb.append(str)
+    }
+    val maxRows = 10
+    val maxCols = 10
+    for (i <- 0 until math.min(numRows, maxRows)) {
+      for (j <- 0 until math.min(numCols, maxCols)) {
+        writeStr(this(i, j).toString)
+      }
+      if (i == 0 && numCols > maxCols)
+        sb.append(" ... (%d Cols)".format(numCols))
+      sb.append("\n")
+    }
+    if (numRows > maxRows)
+      sb.append(" ... (%d Rows)".format(numRows))
+    sb.toString
+  }
 }
 
 object MatrixAdder extends DenseAdders
+trait MatrixAdder[A, Raw, Repr1[_, _], Repr2[_, _], Repr3[_, _]] {
+  def addInPlace(sub: Boolean, m1: Repr1[A, Raw], m2: Repr2[A, Raw], ret: Repr3[A, Raw])
+}
 
-trait MatrixAdder[A, Raw, -Repr1[_, _], -Repr2[_, _], -Repr3[_, _]] {
-  def addInPlace(neg1: Boolean, m1: Repr1[A, Raw], neg2: Boolean, m2: Repr2[A, Raw], ret: Repr3[A, Raw])
+object MatrixMultiplier extends DenseMultipliers
+trait MatrixMultiplier[A, Raw, Repr1[_, _], Repr2[_, _], Repr3[_, _]] {
+  def gemm(alpha: A, beta: A, m1: Repr1[A, Raw], m2: Repr2[A, Raw], ret: Repr3[A, Raw])
 }
 
 object MatrixBuilder extends DenseBuilders
-
-trait MatrixBuilder[A, Raw, +Repr[_, _]] {
+trait MatrixBuilder[A, Raw, Repr[_, _]] {
   def zeros(numRows: Int, numCols: Int): Repr[A, Raw]
 }
 
 
 object Test extends App {
-  val m1 = MatrixBuilder.dense[Float, Float].zeros(4, 4)
+  val m1 = MatrixBuilder.denseRow[Float, Float].zeros(4, 4)
   val m2 = MatrixBuilder.denseRow[Float, Float].zeros(4, 4)
-  val m3 = m1+m2
+  val m3: DenseRow[Float, Float] = m1+m2
   println(m3)
+  val m4 = MatrixBuilder.denseRow[Double, Double].zeros(4, 4)
+  m3.mapTo(_.toDouble, m4)
 
 //  println(implicitly[MatrixAdder[Float, Float, DenseRow, DenseRow, Dense]])
 }
