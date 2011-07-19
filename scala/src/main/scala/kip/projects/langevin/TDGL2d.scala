d6c0d55cd335a93255e235f55bd62fa98dd96b39
Kipton Barros <kbarros@gmail.com>
2011-07-19 17:54:24 -0600

    TDGL generalized to arbitrary dimension,but slow.

--- scala/src/main/scala/kip/projects/langevin/TDGL2d.scala
+++ scala/src/main/scala/kip/projects/langevin/TDGL2d.scala
@@ -16,13 +16,15 @@ object TDGL2d {
 }
 
 
-class TDGL2d(params: Parameters) {
+class TDGL2d(params: Parameters, dimensions: Int) {
   import TDGL2d._
   
   val random = new Random(params.iget("Random seed", 0))
   val L = params.fget("L")
   var t = 0.0
   var dt = params.fget("dt")
+  var r = params.fget("r")
+  
   val (lp, dx) = {
     var dx = params.fget("dx")
     val lp = (L / dx).toInt
@@ -30,60 +32,57 @@ class TDGL2d(params: Parameters) {
     params.set("dx", dx)
     (lp, dx)
   }
-  val phi = new Array[Double](lp*lp)
-  val scratch1 = new Array[Double](lp*lp)
-  val scratch2 = new Array[Double](lp*lp)
-  val fft = new FFTReal(Array(lp, lp), Some(Array(L, L)))
+  
+  val dim = Array.fill(dimensions)(lp)
+  val len = Array.fill(dimensions)(L)
+  val lpd = dim.product
+  
+  val phi = new Array[Double](lpd)
+  val scratch1 = new Array[Double](lpd)
+  val scratch2 = new Array[Double](lpd)
+  val fft = new FFTReal(dim, Some(len))
   
   randomize()
   
   
   def randomize() {
-    for (i <- 0 until lp*lp)
+    for (i <- 0 until lpd)
       phi(i) = 0.1*random.nextGaussian()
     t = 0
   }
   
   def randomizeAndShift() {
     randomize()
-    var sum = 0.0
-    for (i <- 0 until lp*lp)
-      sum += phi(i)
-    for (i <- 0 until lp*lp)
-      phi(i) -= sum / (lp*lp)
+    var sum = phi.sum
+    for (i <- 0 until lpd)
+      phi(i) -= sum / (lpd)
     
-    sum = 0
-    for (i <- 0 until lp*lp)
-      sum += phi(i)
-    if (sum > 1e-8 || sum < -1e-8) {
-      System.out.println("Error in shifting!")
-    }
-
-    t = 0
+    sum = phi.sum
+    if (sum > 1e-8 || sum < -1e-8)
+      println("Error in shifting!")
   }
 
   def randomizeIsing() {
-    val lp2 = lp*lp 
-    val indices = new Array[Int](lp2)
-    for (i <- 0 until lp2)
+    val indices = new Array[Int](lpd)
+    for (i <- 0 until lpd)
       indices(i) = i
     // randomize indices
-    for (i <- 0 until lp2) {
-      val r = i+random.nextInt(lp2-i)
+    for (i <- 0 until lpd) {
+      val r = i+random.nextInt(lpd-i)
       // swap indices(i) and indices(r)
       val that = indices(r)
       indices(r) = indices(i)
       indices(i) = that
     }
-    for (i <- 0 until lp2) {
-      phi(indices(i)) = if (i < lp2/2) -1 else +1
+    for (i <- 0 until lpd) {
+      phi(indices(i)) = if (i < lpd/2) -1 else +1
     }
 
     var sum = 0.0
-    for (i <- 0 until lp2)
+    for (i <- 0 until lpd)
       sum += phi(i)
     if (sum != 0.0) {
-      System.out.println("Error in randomizing!")
+      println("Error in randomizing!")
     }
     
     t = 0
@@ -91,22 +90,22 @@ class TDGL2d(params: Parameters) {
   
   def readParams(params: Parameters) {
     dt = params.fget("dt")
+    r  = params.fget("r")
   }
   
   def r2k2(k: Array[Double]): Double = {
-    val d = k.size
-    require(d == 2)
+    require(k.size == dimensions)
     
     var k2 = 0.0
-    for (i <- 0 until d)
+    for (i <- 0 until dimensions)
       k2 += k(i)*k(i)
 
     var c = 1.0
-    for (i <- 0 until d)
-      c  *= d*k(i)*k(i)/k2
+    for (i <- 0 until dimensions)
+      c *= dimensions*k(i)*k(i)/k2
 
     val a = 0.5
-    if (k2 == 0) 0 else ((1-a)+a*c)*k2
+    if (k2 == 0) 0 else r*r*((1-a)+a*c)*k2
   }
 
   def simulate() {
@@ -118,7 +117,7 @@ class TDGL2d(params: Parameters) {
     }
     
     // scratch2 stores term proportional to phi^3
-    for (i <- 0 until lp*lp) {
+    for (i <- 0 until lpd) {
       scratch2(i) = phi(i)*phi(i)*phi(i)
     }
     fft.convolveWithFn(scratch2, scratch2) { k: Array[Double] =>
@@ -127,7 +126,7 @@ class TDGL2d(params: Parameters) {
       (re, 0)
     }
 
-    for (i <- 0 until lp*lp) {
+    for (i <- 0 until lpd) {
       phi(i) = scratch1(i) - scratch2(i)
     }
     
@@ -142,15 +141,14 @@ class TDGL2d(params: Parameters) {
       (1-k2, 0)
     }
     var ret = 0.0
-    for (i <- 0 until lp*lp) {
+    for (i <- 0 until lpd) {
       ret += - phi(i)*scratch1(i)/2      // - phi (1 + \del^2) phi / 2 
       ret += phi(i)*phi(i)*phi(i)*phi(i) / 4	// phi^4 / 4
     }
     
     // shift energy so that it is zero in the ground state
-    ret += lp*lp/4 
+    ret += lpd/4 
     
-    ret*dx*dx
+    ret*math.pow(dx, dimensions)
   }
-
 }
--- scala/src/main/scala/kip/projects/langevin/TDGL2dApp.scala
+++ scala/src/main/scala/kip/projects/langevin/TDGL2dApp.scala
@@ -18,6 +18,7 @@ class TDGL2dApp extends Simulation {
   def load(c: Control) {
     c.frame(grid)
     params.addm("dt", 1.0)
+    params.addm("r", 1.0);
     params.add("L", 100.0)
     params.add("Random seed", 0)
     params.add("dx", 1.0)
@@ -42,7 +43,7 @@ class TDGL2dApp extends Simulation {
   }
   
   def run() {
-    sim = new TDGL2d(params)
+    sim = new TDGL2d(params, dimensions=2)
     // sim.randomize()
     Job.animate()
     
