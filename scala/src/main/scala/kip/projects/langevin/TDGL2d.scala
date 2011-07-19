ed5cac9a314384bb4cb6724318aa86fd16fab84c
Kipton Barros <kbarros@gmail.com>
2011-07-19 16:53:27 -0600

    Fix FFTReal and use it in TDGL2d

--- scala/src/main/scala/kip/projects/langevin/TDGL2d.scala
+++ scala/src/main/scala/kip/projects/langevin/TDGL2d.scala
@@ -2,42 +2,40 @@ package kip.projects.langevin
 
 import java.util.Random
 import scikit.jobs.params.Parameters
-import scikit.numerics.fft.FFT2D
+//import scikit.numerics.fft.FFT2D
+import kip.math.fft.FFTReal
 import scikit.numerics.fn.Function2D
 
-class TDGL2d(params: Parameters) {
-  val random = new Random(params.iget("Random seed", 0))
-  
-  val L = params.fget("L")
-  
-  var t = 0.0
-  var dt = params.fget("dt")
-  
-  val (x, y) = (1, 2)
-    val (lp, dx) = {
-      var dx = params.fget("dx")
-      val lp = (L / dx).toInt
-      dx = L / lp
-      params.set("dx", dx)
-      (lp, dx)
-    }
-  
-  val phi = new Array[Double](lp*lp)
-  val scratch1 = new Array[Double](lp*lp)
-  val scratch2 = new Array[Double](lp*lp)
-  
-  val fft = new FFT2D(lp, lp)
-  fft.setLengths(L, L)
-  
-  randomize()
-  
-  
+object TDGL2d {
   // constants from Cheng & Rutenberg
   val a1 = 3.0
   val a2 = 0.0
   
   // approximate energy at a single interface per unit area
   val surfaceEnergyDensity = 1.0 // 0.89
+}
+
+
+class TDGL2d(params: Parameters) {
+  import TDGL2d._
+  
+  val random = new Random(params.iget("Random seed", 0))
+  val L = params.fget("L")
+  var t = 0.0
+  var dt = params.fget("dt")
+  val (lp, dx) = {
+    var dx = params.fget("dx")
+    val lp = (L / dx).toInt
+    dx = L / lp
+    params.set("dx", dx)
+    (lp, dx)
+  }
+  val phi = new Array[Double](lp*lp)
+  val scratch1 = new Array[Double](lp*lp)
+  val scratch2 = new Array[Double](lp*lp)
+  val fft = new FFTReal(Array(lp, lp), Some(Array(L, L)))
+  
+  randomize()
   
   
   def randomize() {
@@ -95,34 +93,39 @@ class TDGL2d(params: Parameters) {
     dt = params.fget("dt")
   }
   
-  def r2k2(kx: Double, ky: Double): Double = {
-    val k2 = kx*kx + ky*ky
-    val c = 4*(kx*kx*ky*ky)/(k2*k2)
+  def r2k2(k: Array[Double]): Double = {
+    val d = k.size
+    require(d == 2)
+    
+    var k2 = 0.0
+    for (i <- 0 until d)
+      k2 += k(i)*k(i)
+
+    var c = 1.0
+    for (i <- 0 until d)
+      c  *= d*k(i)*k(i)/k2
+
     val a = 0.5
     if (k2 == 0) 0 else ((1-a)+a*c)*k2
   }
 
   def simulate() {
     // scratch1 stores term proportional to phi
-    fft.convolve(phi, scratch1, new Function2D() {
-      def eval(kx: Double, ky: Double): Double = {
-	//double k2 = kx*kx + ky*ky
-	val k2 = r2k2(kx, ky)
-	(1 + a1*dt + a2*dt*(-k2)) / (1 + (a1-1)*dt + (a2-1)*dt*(-k2))
-      }
-    })
+    fft.convolveWithFn(phi, scratch1) { k: Array[Double] =>
+      val k2 = r2k2(k)
+      val re = (1 + a1*dt + a2*dt*(-k2)) / (1 + (a1-1)*dt + (a2-1)*dt*(-k2))
+      (re, 0)
+    }
     
     // scratch2 stores term proportional to phi^3
     for (i <- 0 until lp*lp) {
       scratch2(i) = phi(i)*phi(i)*phi(i)
     }
-    fft.convolve(scratch2, scratch2, new Function2D() {
-      def eval(kx: Double, ky: Double): Double = {
-	// double k2 = kx*kx + ky*ky
-	val k2 = r2k2(kx,ky)
-	dt / (1 + (a1-1)*dt + (a2-1)*dt*(-k2))
-      }
-    })
+    fft.convolveWithFn(scratch2, scratch2) { k: Array[Double] =>
+      val k2 = r2k2(k)
+      val re = dt / (1 + (a1-1)*dt + (a2-1)*dt*(-k2))
+      (re, 0)
+    }
 
     for (i <- 0 until lp*lp) {
       phi(i) = scratch1(i) - scratch2(i)
@@ -134,13 +137,10 @@ class TDGL2d(params: Parameters) {
   // F = \int dx^2 [ - phi (1 + \del^2) phi / 2 + phi^4 / 4 ]  
   def freeEnergy: Double = {
     // scratch1 stores (1 + \del^2) phi
-    fft.convolve(phi, scratch1, new Function2D() {
-      def eval(kx: Double, ky: Double): Double = {
-	//double k2 = kx*kx + ky*ky
-	val k2 = r2k2(kx, ky)
-	1 - k2
-      }
-    })
+    fft.convolveWithFn(phi, scratch1) { k: Array[Double] =>
+      val k2 = r2k2(k)
+      (1-k2, 0)
+    }
     var ret = 0.0
     for (i <- 0 until lp*lp) {
       ret += - phi(i)*scratch1(i)/2      // - phi (1 + \del^2) phi / 2 
