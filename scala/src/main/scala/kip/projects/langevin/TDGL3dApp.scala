76053034769df5e82fcc2941804882b82027c617
Kipton Barros <kbarros@gmail.com>
2011-07-19 23:15:27 -0600

    2d and 3d tdgl apps both working

--- scala/src/main/scala/kip/projects/langevin/TDGL3dApp.scala
+++ scala/src/main/scala/kip/projects/langevin/TDGL3dApp.scala
@@ -1,27 +1,27 @@
 package kip.projects.langevin
 
 import scikit.util.Utilities.format
-import scikit.graphics.dim2.Grid
+import scikit.graphics.dim3.Grid3D
 import scikit.jobs.Control
 import scikit.jobs.Job
 import scikit.jobs.Simulation
 
 
-object TDGL2dApp extends App {
-  new Control(new TDGL2dApp(), "Allen Cahn Coarsening")
+object TDGL3dApp extends App {
+  new Control(new TDGL3dApp(), "Allen Cahn Coarsening")
 }
 
-class TDGL2dApp extends Simulation {
-  val grid = new Grid("Grid")
+class TDGL3dApp extends Simulation {
+  val grid = new Grid3D("Grid")
   var sim: TDGL2d = _
-
+  
   def load(c: Control) {
     c.frame(grid)
-    params.addm("dt", 1.0)
-    params.addm("r", 1.0);
-    params.add("L", 500.0)
-    params.add("Random seed", 0)
-    params.add("dx", 1.0)
+    params.addm("dt", 5.0)
+    params.addm("r", 1.0)
+    params.add("L", 15.0)
+    params.add("Random seed", 41)
+    params.add("dx", 0.5)
     params.add("Time")
     params.add("Energy")
   }
@@ -31,8 +31,8 @@ class TDGL2dApp extends Simulation {
     
     grid.setScale(-1, 1)
     val lp = sim.lp
-    grid.registerData(lp, lp, sim.phi)
-
+    grid.registerData(lp, lp, lp, sim.phi)
+    
     params.set("Time", format(sim.t))
     params.set("Energy", format(sim.freeEnergy))
   }
@@ -42,13 +42,14 @@ class TDGL2dApp extends Simulation {
   }
   
   def run() {
-    sim = new TDGL2d(params, dimensions=2)
+    sim = new TDGL2d(params, dimensions=3)
     Job.animate()
     
     while (true) {
-      for (i <- 0 until 10)
+      for (i <- 0 until 5)
 	sim.simulate()
       Job.animate()
     }
   }
 }
+
