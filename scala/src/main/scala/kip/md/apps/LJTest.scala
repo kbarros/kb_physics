c078b5b53f7e6d52391139aa20bc2414d70817a5
Kipton Barros <kbarros@gmail.com>
2011-07-20 18:03:10 -0600

    more features in ljtest, some cleanup

--- scala/src/main/scala/kip/md/apps/LJTest.scala
+++ scala/src/main/scala/kip/md/apps/LJTest.scala
@@ -3,6 +3,7 @@ package kip.md.apps
 import java.awt._
 import kip.md._
 import kip.math.{Vec3, mutable}
+import scala.collection.mutable.ArrayBuffer
 import scala.math._
 
 object LJTest {
@@ -10,6 +11,7 @@ object LJTest {
 /*
 {
 "layers": 2
+"rows1": 80
 "cols1": 80
 "sizeAsymmetry": 1.0
 }
@@ -78,6 +80,8 @@ object LJTest {
 
     val (atoms1, atoms2) = surfacePairs.unzip
     val surfaceAtoms = (atoms1 ++ atoms2).distinct
+
+/*
     val grouper = new Grouper(surfaceAtoms)
     for ((a1, a2) <- surfacePairs) grouper.bond(a1, a2)
     
@@ -92,6 +96,8 @@ object LJTest {
     
     val variances = meanVar.unzip._2
     avg(variances)
+*/
+    surfaceAtoms.size
   }
   
   def test3(layers: Int, rows1: Int, cols1: Int, sizeAsymmetry: Double) {
@@ -119,16 +125,12 @@ object LJTest {
     val integrator = new Verlet(dt=0.02, thermostat=thermoDamp)
     
 
-//    val sizeAsymmetry = 1.0
     val r1 = pow(2, 1./6) / 2 // radius of smaller atom
     val r2 = r1 * sizeAsymmetry
 
-//    val rows1 = 20
-//    val cols1 = 80
     val rows2 = rows1
     val cols2 = (cols1/sizeAsymmetry).toInt
     
-//    val layers = 2
     val layerWidth = sqrt(3)*(r1*rows1 + r2*rows2)
     
     val natoms1 = layers*rows1*cols1
@@ -161,14 +163,22 @@ object LJTest {
     viz.scene.translation = Vec3(0, 0, 0.5) // zoom in for improved movie frames
     viz.setBounds(volume.bounds)
 
+    val viz2 = new Visualizer()
+    viz2.scene.translation = Vec3(0, 0, 0.5) // zoom in for improved movie frames
+    viz2.setBounds(volume.bounds)
+    val streamsTargetNum = 300
+    val streamIndices = Seq.fill(streamsTargetNum)(rand.nextInt(atoms.size)).distinct
+    val stream = streamIndices.map { i => (i, ArrayBuffer[Vec3](Vec3.zero, Vec3.zero)) }
+    
     val forcePlot = new scikit.graphics.dim2.Plot("Force")
     val roughPlot = new scikit.graphics.dim2.Plot("Roughening")
-    val disloPlot = new scikit.graphics.dim2.Plot("Dislocations pair correlation")
+    val disloPlot = new scikit.graphics.dim2.Plot("Dislocations")
     scikit.util.Utilities.frame(forcePlot.getComponent(), forcePlot.getTitle())
     scikit.util.Utilities.frame(roughPlot.getComponent(), roughPlot.getTitle())
     scikit.util.Utilities.frame(disloPlot.getComponent(), disloPlot.getTitle())
     val forceHistory = new scikit.dataset.DynamicArray()
     val roughHistory = new scikit.dataset.DynamicArray()
+    val disloHistory = new scikit.dataset.DynamicArray()
     val disloData = new DislocationAnalysis(tmin=0, tmax=maxTime, dt=100, rmin=0, rmax=50, dr=1, volume=1)
     
     def isType1(idx: Int): Boolean = idx < natoms1
@@ -209,6 +219,9 @@ object LJTest {
       initGrid(x0=off, y0=off+layer*layerWidth+(sqrt(3)*r1)*rows1,
                r=r2, rows=rows2, cols=cols2, atoms.view(natoms1+layer*rows2*cols2, natoms1+(layer+1)*rows2*cols2))
     }
+    for ((i, pts) <- stream) { 
+      pts(0) = atoms(i).pos.copy()
+    }
     
     def averageWallForce(): Double = {
       val inter1s = atoms.flatMap(_.tag.inter1).distinct
@@ -217,22 +230,37 @@ object LJTest {
       wallForce
     }
     
+    def hasWrapped() = atoms.find(_.wx != 0).isDefined
+
+
+    // =======================
+    // Main loop
+    //
     var iter = 0
-    while (world.time < maxTime) kip.util.Util.time2("Iterating") {
+    while (world.time < maxTime && !hasWrapped()) kip.util.Util.time2("Iterating") {
+      // ----------------
+      // World step
+      //
       iter += 1
       val wallDelta = wallSpeed*world.time
       val wall1pos = wall1posInit + wall1norm*wallDelta
       val wall2pos = wall2posInit + wall2norm*wallDelta
       setInteractions(sizeAsymmetry, wall1pos, wall2pos)
-      
       world.step(20)
-
-      forceHistory.append2(world.time, averageWallForce())
-      forcePlot.registerLines("Data", forceHistory, java.awt.Color.BLACK)
-      
       val cutoff = 1.9
       val neighbors = neighborList(atoms, volume, cutoff)
 
+      
+      // ----------------
+      // Force plot
+      //
+      forceHistory.append2(world.time, averageWallForce())
+      forcePlot.registerLines("Data", forceHistory, java.awt.Color.BLACK)
+      
+
+      // ----------------
+      // Dislocations plot
+      //
       val dislocationPairs = {
         for (i <- atoms.indices;
              if (neighbors(i).size == 7);
@@ -243,15 +271,20 @@ object LJTest {
           (atoms(i), atoms(jmin))
         }
       }
-      analyzeDislocations(world.time, disloData, dislocationPairs)
-      val disloResults = disloData.results
-      for (i <- disloResults.indices) {
-        val (t, g) = disloResults(i)
-        import java.awt.Color._
-        val colors = Seq(BLACK, BLUE, RED, ORANGE, GRAY, YELLOW, GREEN)
-        disloPlot.registerLines("Dislo "+t, new scikit.dataset.PointSet(g.elemCenters, g.elems), colors(i % colors.size))
-      }
-      
+      // analyzeDislocations(world.time, disloData, dislocationPairs)
+      // val disloResults = disloData.results
+      // for (i <- disloResults.indices) {
+      //   val (t, g) = disloResults(i)
+      //   import java.awt.Color._
+      //   val colors = Seq(BLACK, BLUE, RED, ORANGE, GRAY, YELLOW, GREEN)
+      //   disloPlot.registerLines("Dislo "+t, new scikit.dataset.PointSet(g.elemCenters, g.elems), colors(i % colors.size))
+      // }
+      disloHistory.append2(world.time, dislocationPairs.size)
+      disloPlot.registerLines("Dislo", disloHistory, java.awt.Color.BLACK)
+
+      // ----------------
+      // Roughening plot
+      //
       val surfacePairs = {
         for (i <- atoms.indices;
              if (isType1(i));
@@ -262,8 +295,11 @@ object LJTest {
       roughHistory.append2(world.time, roughening)
       roughPlot.registerLines("Data", roughHistory, java.awt.Color.BLACK)
       
-      viz.setString("Time=%d, Atoms=%d, Temp=%f".format(world.time.toInt, atoms.size, world.temperature()))
       
+      // ----------------
+      // Atoms display
+      //
+      viz.setString("Time=%d, Atoms=%d, Temp=%f".format(world.time.toInt, atoms.size, world.temperature()))
       viz.setParticles(atoms.indices.map { i =>
         val a = atoms(i)
         val r = if (isType1(i)) r1 else r2
@@ -275,15 +311,29 @@ object LJTest {
         }
         Visualizer.Sphere(a.pos, radius=0.8*r, color=c)
       })
-      
       viz.setWalls(Seq(
         Visualizer.Wall(wall1norm, wall1pos, Color.ORANGE),
         Visualizer.Wall(wall2norm, wall2pos, Color.ORANGE)
       ))
-      
       viz.display()
-
       javax.imageio.ImageIO.write(viz.scene.captureImage(), "PNG", new java.io.File(dirname+"/snap%d.png".format(iter)))
+      
+      
+      // ----------------
+      // Displacement display
+      //
+      for ((i, pts) <- stream) { 
+        pts(pts.size-1) = atoms(i).pos.copy()
+      }
+      viz2.streams = stream.map { case (i, pts) =>
+        val p0 = pts.head
+        val p1 = pts.last
+        val scale = 4
+        val del = (p0 - p1) / scale
+        Visualizer.Stream(pts=Seq(p1+del, p1),
+                          color=(if (isType1(i)) Color.BLUE else Color.RED))
+      }
+      viz2.display()
     }
   }
 
