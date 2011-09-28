d3cde4672a6fcf38e2ec98469f4dcd28842b6b39
Kipton Barros <kbarros@gmail.com>
2011-09-28 16:48:08 -0600

    More dynamic ANNNI simulation.

--- scala/src/main/scala/kip/projects/spinglass/Annni2d.scala
+++ scala/src/main/scala/kip/projects/spinglass/Annni2d.scala
@@ -15,20 +15,26 @@ class Annni2dSim(params: Parameters) {
 
   val L = params.iget("L");
   val N = L*L;
-  val occupiedFraction = params.fget("Occupied fraction");
   
   val spin = new Array[Int](N)
-  val occupied = new Array[Int](N) // 1 if occupied, 0 if vacant
+  val occupied = new Array[Int](N) // occupation priority indexed by spin
   val field = new Array[Double](N)
   
   random.setSeed(params.iget("Random seed"));
   for (i <- 0 until N) {
     spin(i) = if (random.nextDouble() < 0.5) 1 else -1
     field(i) = random.nextGaussian() 
-    occupied(i) = if (random.nextDouble() < occupiedFraction) 1 else 0
-    if (occupied(i) == 0) spin(i) = 0
+    occupied(i) = i
   }
-
+  for (i <- 0 until N-1) {
+    val k = i + random.nextInt(N - i)
+    // swap occupied(i) and occupied(k)
+    val t = occupied(i)
+    occupied(i) = occupied(k)
+    occupied(k) = t
+  }
+  
+  var occupiedCnt: Int = _
   var T: Double = _
   var fieldMean: Double = _
   var fieldStrength: Double = _
@@ -41,6 +47,14 @@ class Annni2dSim(params: Parameters) {
     fieldMean = params.fget("Field mean")
     fieldStrength = params.fget("Field std. dev.")
     J(1) = params.fget("J2 / J1")
+    occupiedCnt = (params.fget("Occupied fraction") * N).toInt max 0 min (N-1)
+    
+    for (i <- 0 until N) {
+      if (occupied(i) <= occupiedCnt && spin(i) == 0)
+        spin(i) = if (random.nextDouble() < 0.5) 1 else -1
+      else if (occupied(i) > occupiedCnt && spin(i) != 0)
+        spin(i) = 0
+    }
   }
   
   def buildNeighbors(i: Int): (Array[Int], Array[Int]) = {
@@ -51,11 +65,15 @@ class Annni2dSim(params: Parameters) {
       (-1, 0, 0),
       (0, -1, 0),
       
+//      // anti-ferrogment nnn
+//      (+1, +1, 1),
+//      (-1, +1, 1),
+//      (+1, -1, 1),
+//      (-1, -1, 1)
+      
       // anti-ferrogment nnn
-      (+1, +1, 1),
-      (-1, +1, 1),
-      (+1, -1, 1),
-      (-1, -1, 1)
+      (+2, 0, 1),
+      (-2, 0, 1)
     )
     
     val y = i/L
@@ -77,14 +95,14 @@ class Annni2dSim(params: Parameters) {
     for (nidx <- 0 until n.size) {
       val j = n(nidx)
       val a = J(ji(nidx))
-      acc += occupied(i)*occupied(j)*a*spin(i)*spin(j)
+      acc += a*spin(i)*spin(j)
     }
     acc
   }
     
   // energy contribution between spin i and external field
   def fieldEnergy(i: Int): Double = {
-    - (occupied(i) * spin(i)) * (fieldMean + fieldStrength * field(i))
+    - spin(i) * (fieldMean + fieldStrength * field(i))
   }
     
   def netEnergy(): Double = {
@@ -123,14 +141,14 @@ class Annni2d extends Simulation {
   def load(c: Control) {
     grid.setScale(-1, +1);
     c.frame(grid);
-    params.add("L", 32);
+    params.add("L", 64);
     params.add("Random seed", 0);
-    params.add("Occupied fraction", 1.0);
     
-    params.addm("T",               new DoubleValue(0, 0, 10).withSlider());
+    params.addm("T",               new DoubleValue(1, 0, 2).withSlider());
     params.addm("J2 / J1",         new DoubleValue(0.5, 0.1, 1).withSlider())
     params.addm("Field mean",      new DoubleValue(0, -10, 10).withSlider())
     params.addm("Field std. dev.", new DoubleValue(0, 0, 2).withSlider())
+    params.addm("Occupied fraction", new DoubleValue(1, 0, 1).withSlider())
   }
 
   def animate() {
@@ -146,7 +164,8 @@ class Annni2d extends Simulation {
     sim = new Annni2dSim(params);    
 
     while (true) {
-      sim.step();           
+      for (i <- 0 until 1000)
+        sim.step();
       Job.animate();
     }
   }
