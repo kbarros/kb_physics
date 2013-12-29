e0680b29e56a124d359cfab12a61816c0e0b0b09
Kipton Barros <kbarros@gmail.com>
2013-12-29 16:32:32 -0700

    Spiral initial conditions + tests.

--- src/main/scala/kip/projects/quantum/SquareLattice.scala
+++ src/main/scala/kip/projects/quantum/SquareLattice.scala
@@ -7,11 +7,12 @@ import kip.math.Vec3
 
 object SquareLattice extends App {
   import kip.util.Util.{time}
-  time("integrated density")(testIntegratedDensity())
+  // time("integrated density")(testIntegratedDensity())
+  time("spiral energy")(testSpiralEnergy())
   
   // Plots the integrated density of states
   def testIntegratedDensity() {
-    val q = new SquareLattice(w=16, h=16, t1=1, t2= -0.5, J_H=0.1, e_min= -10, e_max= 10)
+    val q = new SquareLattice(w=20, h=20, t1=1, t2= -0.5, J_H=0.1, e_min= -10, e_max= 10)
     q.setFieldFerro(q.field)
     q.fillMatrix(q.matrix)
     
@@ -25,6 +26,39 @@ object SquareLattice extends App {
     val plot = KPM.mkPlot("Integrated density of states")
     KPM.plotLines(plot, (range, KPM.integrateDeltas(range, KPM.eigenvaluesExact(H), moment=0)), "Exact", java.awt.Color.RED)
     KPM.plotLines(plot, (range, KPM.integrate(range, KPM.eigenvaluesApprox(order, range, kpm), moment=0)), "Approx", java.awt.Color.BLACK)
+  }
+  
+  def testSpiralEnergy() {
+    val mu = -2.5
+    
+    def energyPerSite(q: KondoHamiltonian) = {
+      val eigs = KPM.eigenvaluesExact(q.matrix).map(_ * q.e_scale) 
+      eigs.takeWhile(_ <= mu).map(_.re - mu).sum / q.numLatticeSites
+    }
+
+    def testJ(J: Double) {
+      println(s"Using J=$J")
+      
+      println("d=6")
+      for (L <- Seq(12, 18, 24, 30, 36)) {
+        val q = new SquareLattice(w=L, h=L, t1=1, t2= -0.5, J_H=J, e_min= -10, e_max=10)
+        q.setFieldSpiral(q.field, 6)
+        q.fillMatrix(q.matrix)
+        println(s"L=$L, e=${energyPerSite(q)}")
+      }
+
+      println("d=8")
+      for (L <- Seq(8, 16, 24, 32, 40)) {
+        val q = new SquareLattice(w=L, h=L, t1=1, t2= -0.5, J_H=J, e_min= -10, e_max=10)
+        q.setFieldSpiral(q.field, 8)
+        q.fillMatrix(q.matrix)
+        println(s"L=$L, e=${energyPerSite(q)}")
+      }
+    }
+    
+    // testJ(0.1)
+    // testJ(0.05)
+    // testJ(0)
   }
 }
 
@@ -44,8 +78,24 @@ class SquareLattice(val w: Int, val h: Int, val t1: R, val t2: R, val J_H: R, va
   def setField(desc: String) {
     desc match {
       case "ferro"    => setFieldFerro(field)
+      case s if desc.startsWith("spiral") => {
+        setFieldSpiral(field, desc.replace("spiral", "").toInt)
+      }
     }
   }
+    
+  def setFieldSpiral(field: Array[R], d: Int) {
+    require(w % d == 0)
+    require(h % d == 0)
+    for (y <- 0 until h;
+         x <- 0 until w) {
+      val i = coord2idx(x, y)      
+      field(fieldIndex(0, i)) = math.cos(2*math.Pi * (x+y) / d)
+      field(fieldIndex(1, i)) = math.sin(2*math.Pi * (x+y) / d)
+      field(fieldIndex(2, i)) = 0
+    }
+  }
+
   
   lazy val latticePositions: Array[Vec3] = {
     (for (i <- 0 until numLatticeSites) yield { 
