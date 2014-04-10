11a13c0817708cb050599bdcbd81c9d8b2ca9925
Kipton Barros <kbarros@gmail.com>
2014-04-10 11:47:50 -0600

    Extend visualization, based off MolViz.

--- src/main/scala/kip/projects/quantum/qmd/TbViz.scala
+++ src/main/scala/kip/projects/quantum/qmd/TbViz.scala
@@ -1,6 +1,5 @@
 package kip.projects.quantum.qmd
 
-import kip.util.{Snapshot, LammpsParser}
 import kip.util.Util.time
 import kip.util.Interpreter
 import kip.math.{Vec3}
@@ -15,6 +14,9 @@ import scikit.graphics.dim2.Plot
 import kip.projects.quantum.kpm.KPMUtil
 import kip.projects.quantum.kpm.EnergyScale
 import scikit.dataset.PointSet
+import kip.util.RangeArray
+import kip.util.Snapshot
+
 
 object TbViz {
   def readSnap(file: File): TbMD.Snap = {
@@ -30,6 +32,26 @@ object TbViz {
     (files zip indices).sortBy(_._2).filter(_._2 % readEvery == 0).map(x => readSnap(x._1))
   }
   
+  def convertToLammpsSnapshot(snap: TbMD.Snap): Snapshot = {
+    val natoms = snap.x.size / 3
+    val ret = new Snapshot(time = 0, natoms)
+    ret.lo = Vec3(snap.bdsLo(0), snap.bdsLo(1), snap.bdsLo(2))
+    ret.hi = Vec3(snap.bdsHi(0), snap.bdsHi(1), snap.bdsHi(2))
+    ret.id  = Array.tabulate(natoms)(i=>i.toDouble)
+    ret.typ = Array.fill(natoms)(0.0)
+    val pos = snap.x.grouped(3).toArray
+    ret.x = pos.map(_(0)).toArray
+    ret.y = pos.map(_(1)).toArray
+    ret.z = pos.map(_(2)).toArray
+    // ix, iy, iz
+    val vel = snap.v.grouped(3)
+    ret.vx = vel.map(_(0)).toArray
+    ret.vy = vel.map(_(1)).toArray
+    ret.vz = vel.map(_(2)).toArray
+    // q
+    ret
+  }
+  
   def main(args: Array[String]) {
     if (args.size != 2) {
       println("Usage: TbViz <dirname> <readevery>")
@@ -37,98 +59,88 @@ object TbViz {
     val dir = args(0)
     val readEvery = args(1).toInt
     val snaps = readSnaps(new java.io.File(dir+"/dump"), readEvery)
-    new TbViz(snaps)
+    val snaps2 = snaps.map(convertToLammpsSnapshot(_))
+    val mv = new MolViz(snaps2, RenderProperties.basic)
+    // new TbViz(snaps)
+    
+    val dv = new DensityViz(snaps)
+    val pv = new PairViz(snaps2)
+    mv.callbacks :+= (dv.display(_)) 
+    mv.callbacks :+= (pv.display(_))
+    mv.goto(0)
+  }
+}
+
+class DensityViz(snaps: Array[TbMD.Snap]) {
+  val plot = {
+    val ret = new Plot("Integrated density")
+    scikit.util.Utilities.frame(ret.getComponent(), ret.getTitle())
+    ret
+  }
+  
+  def display(idx: Int) {
+    val snap = snaps(idx)
+    val es = new EnergyScale(snap.energyScale._1, snap.energyScale._2)
+    val gamma = KPMUtil.momentTransform(snap.moments, 10*snap.moments.size)
+    val (xp, irho) = KPMUtil.integratedDensityFunction(gamma, es)
+    val data = new PointSet(xp, irho)
+    plot.registerLines("Integrated density", data, Color.BLACK)
+    val muIdx = xp.indexWhere(_ > snap.mu)
+    val muData = new PointSet(Array(snap.mu), Array(irho(muIdx)))
+    plot.registerBars("Mu", muData, Color.RED)
   }
 }
 
 
-class TbViz(val snaps: Seq[TbMD.Snap]) {
-  var idx: Int = 0
-  
-  val scene = new Scene() {
-    def drawContent(gfx: GfxGL) {
-      val snap = snaps(idx)
-      val bds = {
-        val lo = Vec3(snap.bdsLo(0), snap.bdsLo(1), snap.bdsLo(2))
-        val hi = Vec3(snap.bdsHi(0), snap.bdsHi(1), snap.bdsHi(2))
-        Bounds3d(lo, hi)
-      }
-      gfx.perspective3d(bds, rotation, translation*(bds.hi-bds.lo).norm)
-      gfx.setColor(Color.GREEN)
-      gfx.drawCuboid(bds)
-      for (i <- 0 until snap.x.size/3) {
-        val pos = Vec3(snap.x(3*i+0), snap.x(3*i+1), snap.x(3*i+2))
-        gfx.setColor(Color.BLUE)
-        gfx.drawSphere(pos, 1*Units.angstrom, 2)
-      }
-      
-      gfx.ortho2dPixels()
-      gfx.setColor(Color.RED)
-      val pixOffset = 4
-      gfx.rasterString(f"i=$idx/${snaps.size-1} t=${snap.time/Units.fs}%g (fs) energy=${snap.energy/Units.eV}%g (eV)", pixOffset, pixOffset)
-
-    }
-  }
-  
-  val frame = {
-    val frame = new Frame("Atoms")
-    frame.addWindowListener(new WindowAdapter() {
-      override def windowClosing(e: WindowEvent) {
-        System.exit(0)
-      }
-    })
-
-    val slider = new JSlider(0, snaps.size-1, 0)
-    slider.addChangeListener(new ChangeListener() {
-      def stateChanged(e: ChangeEvent) {
-        goto(slider.getValue)
-        scene.display()
-      }
-    })
+class PairViz(snaps: Array[Snapshot]) {
+  def pairCorrelation(snaps: Seq[Snapshot], dr: Double, rmax: Double, typs1: Seq[Int], typs2: Seq[Int]): RangeArray[Double] = {
+    val volume = snaps(0).volume
+    val g = RangeArray.fill(xmin=0, xmax=rmax, dx=dr)(0d)
+    // sum over all snapshots, and all pairs of particles. distances are binned into g
+    var uniquePairs = 0
+    var atomsCnt = 0
     
-    val panel = new JPanel(new BorderLayout())
-    panel.add(scene.component, BorderLayout.CENTER);
-    panel.add(slider, BorderLayout.SOUTH);
+    val sameTyps = typs1.toSet == typs2.toSet
     
-    frame.add(panel)
-    frame.setSize(300, 300)
-    frame.setVisible(true)
-    frame
-  }
-  
-  lazy val plot = {
-    val plot = new Plot("Integrated density")
-    scikit.util.Utilities.frame(plot.getComponent(), plot.getTitle())
-    plot
-  }
-  
-  def goto(i: Int) {
-    idx = i
-    scene.display()
-    
-    // integrated density 
-    if (true) {
-      val snap = snaps(idx)
-      val es = new EnergyScale(snap.energyScale._1, snap.energyScale._2)
-      val gamma = KPMUtil.momentTransform(snap.moments, 10*snap.moments.size)
-      val (xp, irho) = KPMUtil.integratedDensityFunction(gamma, es)
-      val data = new PointSet(xp, irho)
-      plot.registerLines("Integrated density", data, Color.BLACK)
-      val muIdx = xp.indexWhere(_ > snap.mu)
-      val muData = new PointSet(Array(snap.mu), Array(irho(muIdx)))
-      plot.registerBars("Mu", muData, Color.RED)
+    for ((s,iter) <- snaps.zipWithIndex) {
+      val ids1 = (0 until s.natoms) filter (i => typs1.contains(s.typ(i)))
+      val ids2 = (0 until s.natoms) filter (i => typs2.contains(s.typ(i)))
+      // if (iter % 100 == 0) println("Processing snapshot "+iter)
+      for (i1 <- ids1; i2 <- ids2; if i1 != i2) {
+        val r = math.sqrt(s.distance2(i1, i2))
+        if (g.isDefinedAt(r))
+          g(r) += 1
+        uniquePairs += 1
+      }
+      atomsCnt += (ids1 ++ ids2).distinct.size
     }
+    if (false) {
+      for (r <- g.elemCenters) {
+        val volume_fraction = 4*math.Pi*r*r*dr / volume
+        g(r) /= volume_fraction * uniquePairs
+      }
+    }
+    else {
+      var acc = 0.0
+      for (r <- g.elemCenters) {
+        acc += g(r) / atomsCnt
+        g(r) = acc
+      }
+    }
+    g
   }
   
-  goto(0)
   
-  def animate() {
-    for (i <- 500 until 2000) {
-      goto(i)
-      // val im = kip.graphics.Utilities.captureJComponentImage(comp, comp.getWidth(), comp.getHeight())
-      // val im = scene.captureImage()
-      // javax.imageio.ImageIO.write(im, "PNG", new java.io.File("foo6.png"))
+  val plot = {
+    val ret = new Plot("Pair correlation")
+    scikit.util.Utilities.frame(ret.getComponent(), ret.getTitle())
+    ret
+  }
 
-    }
+  def display(idx: Int) {
+    val snap = snaps(idx)
+    val g = pairCorrelation(Seq(snap), dr=0.005, rmax=8, typs1=Seq(0), typs2=Seq(0))
+    val data = new PointSet(g.elemCenters, g.elems)
+    plot.registerLines("Integrated density", data, Color.BLACK)
   }
 }
